d7628def6170a10e257ea5618fd3d396f9b12ec0
ohdcthang <thanghd@coin98.finance>
2024-11-06 10:25:40 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,45 @@
-import Image from "next/image";
+"use client";
+
+import { OrdinalsEngine } from "@/ordinals-sdk/src";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inscriptionId, setInscriptionId] = useState('')
+  const [inscriptionResult, setInscriptionResult] = useState('')
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const engine = new OrdinalsEngine();
+
+  const getInscription = async () => {
+      const data = await engine.getHistory({address:'bc1qgw7f850c7zgvhrhdvv6utlwnr273rfr7xgn0dv', pagination:{
+        limit: 10,
+        page: 1
+      }})
+      console.log("🚀 ~ getInscription ~ data:", data)
+      setInscriptionResult(JSON.stringify(data))
+  }
+
+  return (
+    <div className=" items-center justify-items-center min-h-screen p-12 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-3xl font-semibold">COIN98 ORDINALS SDK</h1>
+
+      {/* GET INSCRIPTION INFO */}
+      <div className="w-full">
+        <div className="flex justify-between w-full">
+          <div className="flex items-center gap-4 justify-between w-full">
+            <p className="font-semibold">GET INSCRIPTION INFORMATION : </p>
+            <p className="font-semibold">RESULT : </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex justify-between w-full pt-4">
+          <div className="flex gap-4 justify-between w-full text-black">
+            <textarea onChange={(e) => setInscriptionId(e?.target?.value)} className="min-h-[100px] w-full" />
+            <textarea value={inscriptionResult} readOnly className="min-h-[100px] w-full" />
+          </div>
+        </div>
+        <button onClick={getInscription} className="text-white px-4 py-2 border-white border mt-2 rounded-xl hover:opacity-90">
+          GET INCRIPTION
+        </button>
+      </div>
     </div>
   );
 }
